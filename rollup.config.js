d47635ad107b20306ca9c52b23c97e07992dccf7
tylde <mr.mkalinowski@gmail.com>
2020-04-06 23:47:04 +0200

    repo init

---
+++ rollup.config.js
@@ -0,0 +1,27 @@
+import clear from 'rollup-plugin-clear';
+import resolve from '@rollup/plugin-node-resolve';
+import typescript from 'rollup-plugin-typescript2';
+
+const plugins = [
+  clear({targets: ['dist']}),
+  resolve(),
+  typescript({
+    abortOnError: false,
+    tsconfig: './tsconfig.json',
+    clean: true
+  })
+];
+
+const inputFile = 'src/main.ts';
+
+const outputFile = 'default/main.js';
+
+export default {
+  input: inputFile,
+  output: {
+    file: outputFile,
+    format: 'cjs',
+    sourcemap: true
+  },
+  plugins
+};
---
+++ src/main.ts
@@ -0,0 +1,10 @@
+export const loop = () => {
+  console.log(`Current game tick is ${Game.time}`);
+
+  // Automatically delete memory of missing creeps
+  for (const name in Memory.creeps) {
+    if (!(name in Game.creeps)) {
+      delete Memory.creeps[name];
+    }
+  }
+};
